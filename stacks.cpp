fd6d613aed1d3565ecb58f18984a0faf905d2376
Cooper Union EE <student@ice07.ee.cooper.edu>
2018-04-04 01:58:07 -0400

    Added it for real

--- stacks.cpp
+++ stacks.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
-int main() {
-	
-	return 0;
+using namespace std;
+
+template <class T>
+class Node {
+	public:
+		T data; //"data" or "value"
+		Node *next;
+		
+};
+
+
+template <class T>
+class SimpleList {
+	public:
+		Node<T> *head;
+		Node<T> *last;
+		string getName();
+
+	private:
+		string name;
+		
+};
+
+SimpleList::getName() {
+	return this->name;
+}
+
+int main(){
+	cout << "Hi" << endl;
 }
